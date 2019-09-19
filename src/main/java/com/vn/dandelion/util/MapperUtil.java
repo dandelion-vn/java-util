514e4268e185d424b2716b08dfa61a56f87d20af
Dao Huy Duc <huyducactvn@gmail.com>
2019-09-19 22:08:22 +0700

    init code

---
+++ src/main/java/com/vn/dandelion/util/MapperUtil.java
@@ -0,0 +1,73 @@
+package com.vn.dandelion.util;
+
+import static com.vn.dandelion.util.ExceptionUtil.getFullStackTrace;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.IOException;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+@Slf4j
+public class MapperUtil {
+
+  private static MapperUtil ourInstance = new MapperUtil();
+
+  private ModelMapper modelMapper;
+
+  private MapperUtil() {
+    modelMapper = new ModelMapper();
+    modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+  }
+
+  private static MapperUtil getInstance() {
+    return ourInstance;
+  }
+
+  public static Map<String, Object> toMap(Object object) {
+    return toMap(toJson(object));
+  }
+
+  public static Map<String, Object> toMap(String json) {
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      return mapper.readValue(json, new TypeReference<Map<String, Object>>() {});
+    } catch (IOException ex) {
+      log.error("(toMap)ex: {}", getFullStackTrace(ex));
+      return null;
+    }
+  }
+
+  public static String toJson(Object object) {
+    if (object == null) {
+      return null;
+    }
+
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    try {
+      return mapper.writeValueAsString(object);
+    } catch (IOException ex) {
+      log.error("(toJson)ex: {}", getFullStackTrace(ex));
+      return object.toString();
+    }
+  }
+
+  public static ModelMapper getModelMapper() {
+    return getInstance().modelMapper;
+  }
+
+  public static ObjectMapper getMapper() {
+    return new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  }
+
+  public static XmlMapper getXmlMapper() {
+    XmlMapper xmlMapper = new XmlMapper();
+    xmlMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    return xmlMapper;
+  }
+}
